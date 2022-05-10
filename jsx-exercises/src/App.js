b056385ff12fb5a1b4fc38a1a4167a5e473d6547
Daniele Canale <danycnc95@gmail.com>
2022-05-10 12:58:54 +0200

    svolto jsx-05

--- jsx-exercises/src/App.js
+++ jsx-exercises/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import MyFunction from './MyFunction';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <MyFunction a={5} b={6} />
     </div>
   );
 }
---
+++ jsx-exercises/src/MyFunction.js
@@ -0,0 +1,10 @@
+const MyFunction = ({ a, b }) => {
+  const sum = a + b;
+  return (
+    <div>
+      <h2>Sum is: {sum}</h2>
+    </div>
+  );
+};
+
+export default MyFunction;
